a36b23d96ee8c3aac8ab867a14adf9a46cac195b
lukekim420 <lukekim420@naver.com>
2023-06-03 02:02:06 +0900

    asf

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-
+const express = requrire('express');
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-app.post('/data', (req, res) => {
-    // Extract data from the request
-    const name = req.body.name;
-    const reactionTimes = req.body.reactionTimes;
-
-    // Here, you would usually store the data in a database.
-    // For now, we'll just log it to the console.
-    console.log(`Received data from ${name}`);
-    console.log(reactionTimes);
-
-    res.send('Data received');
-});
-
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+app.listen(8080,function(){
+    console.log('listening on 8080')
 });
